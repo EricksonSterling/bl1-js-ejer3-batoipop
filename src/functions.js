38df4902cef967a80c0f653ef65be8f896b70d99
EricksonSterlingp <ericksterlinp@gmail.com>
2021-09-29 18:43:48 +0200

    Actualización de la finalización de la primera práctica

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,14 @@
 'use strict';
+const PHOTO_FILE = './media/photos/';
+const DEFAULT_PHOTO = 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg';
 
 function createTemplate(products) {
+
+   
+    const STARS = '<div class="bi-star-fill"></div>';
+
+    let precio_original = transformarMoneda(products.original_price);
+    let precio_descontado = transformarMoneda(products.discount_price);
 
     let productDiv = document.getElementById('products');
     let productTemplate = document.createElement('div');
@@ -11,9 +19,9 @@ function createTemplate(products) {
     `
         <div class="card h-100">
             <!-- Sale badge, sólo si está vendido-->
-            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
+            <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">${products.sale ? 'Sale': ''}</div>
             <!-- Product image-->
-            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="Imagen de _nombre_del_producto_" />
+            <img class="card-img-top" src="${getImg(products.img)}" alt="Imagen de _nombre_del_producto_" />
             <!-- Product details-->
             <div class="card-body p-4">
                 <div class="text-center">
@@ -21,19 +29,16 @@ function createTemplate(products) {
                     <h5 class="fw-bolder">${products.name}</h5>
                     <!-- Product reviews, un div bi-star para cada estrella a pintar-->
                     <div class="d-flex justify-content-center small text-warning mb-2">
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
-                        <div class="bi-star-fill"></div>
+                        ${STARS.repeat(products.stars)}                        
                     </div>
                     <!-- Product price-->
-                    <span class="text-muted text-decoration-line-through">1000.00 €</span>
-                    800.00 €
+                    ${products.discount_price ? '<span class="text-muted text-decoration-line-through">'+precio_descontado+'</span>' : ''}
+                    ${precio_original}
                     <!-- Product details -->
                     <p>
-                        _tipo_de_combustible_ - 
-                        _tipo_de_cambio_
-                        <br>${products.km}
+                    ${getFuel(products.fuel)} - 
+                        ${products.manual_gear ? 'Manual' : 'Automático'}
+                        <br>${products.km.toLocaleString('de-DE')} km
                     </p>
                 </div>
             </div>
@@ -44,11 +49,27 @@ function createTemplate(products) {
         </div>
     `;
 
-
     productDiv.appendChild(productTemplate);
 }
 
 function transformarMoneda(moneda) {
 
-    return new Intl.NumberFormat(es-ES).format(moneda);
+    //.toLocaleString().toFixed()
+    const TRANSFORMAR = new Intl.NumberFormat('de-DE', {
+        style: 'currency',
+        currency: 'EUR'
+     });
+
+    return TRANSFORMAR.format(moneda);
 }
+
+function getImg(img) {
+  
+    return img ? PHOTO_FILE + img : DEFAULT_PHOTO;
+}
+
+function getFuel(fuel) {
+
+    return typesOfFuel.find((element) => element.id === fuel) ? typesOfFuel.find((element) => element.id === fuel).fuel : 'Desconocido';
+}
+
